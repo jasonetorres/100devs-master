7bc7dde7f83ac3ded85beba3e069a2f30f0f0628
jasonetorres <jasontorres585@icloud.com>
2023-08-20 06:30:16 -0400

    array fun!!

--- class 19/bring-it-on-all-or-nothing/js/main.js
+++ class 19/bring-it-on-all-or-nothing/js/main.js
@@ -32,6 +32,21 @@ function alertOrLog(b, str){
         console.log(str)
     }
 }
+//const alertOrLog = (b,str) => b ? alert(str) : console.log(str)
 
 //*Loops*
 //Create a function that takes in a number. Console log all values from 1 to that number, but if the number is divisible by 3 log "fizz" instead of that number, if the number is divisible by 5 log "buzz" instead of the number, and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
+function fizzBuzz(num){
+    for(let i = 1; i <= num; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log('FizzBuzz')
+        }else if(i % 3 === 0) {
+            console.log('Fizz')
+        }else if(i % 5 === 0){
+            console.log('Buzz')
+        }else{
+        console.log(i);
+    }
+}
+}
+//fizzBuzz(30)
