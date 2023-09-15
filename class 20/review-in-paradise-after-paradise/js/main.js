edab4feff2336fedaf7112f7b4f756eca36ad0db
jasonetorres <jasontorres585@icloud.com>
2023-09-15 06:04:54 -0400

    array iteration practice

--- class 20/review-in-paradise-after-paradise/js/main.js
+++ class 20/review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,12 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+
+function checkFirstLast(arr) {
+    if( arr[0] , arr[arr.length -1 ]) {
+        alert('hi')
+        
+    } else if( arr[0] > arr[ arr.length -1 ]){
+        alert('bye')
+    }else{
+        alert('later')
+    }
+}
